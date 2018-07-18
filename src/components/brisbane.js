5032affa2f4e219239a24abbe4028320621df426
Andrew Hase <oldm8reggie@gmail.com>
2018-07-18 19:36:47 +1000

    Successfully called Brisbane weather via API

--- src/components/brisbane.js
+++ src/components/brisbane.js
@@ -9,22 +9,29 @@ class Brisbane extends Component {
             data: []
         };
     }
-  
+
     componentDidMount() {
-        //fetch('https://api.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9')
-        fetch('https://samples.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9',{
-            crossDomain:true,
+        /* fetch('https://api.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9',{
+        //fetch('https://samples.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9', {
+            crossDomain: true,
             method: 'POST',
             mode: "no-cors",
-            headers: {'Content-Type':'application/json'}})
+            headers: { 'Content-Type': 'application/json' }
+        }) */
+        fetch('http://api.apixu.com/v1/forecast.json?key=69e03ca47c2d4a998f593024181807&q=Brisbane&days=1')
             .then(res => res.json())
-            .then(data => this.setState({ data }, () => console.log('data: ', data)));
+            .then(data => this.setState( [data] , () => console.log('data: ', data)))
+
+            .catch(function (error) {
+                console.log('Looks like there was a problem: \n', error);
+            });
+
     }
     render() {
         return (
             <div>
-            <h1>{this.state.title}</h1>
-            <p>{this.state.data}</p>
+                <h1>{this.state.title}</h1>
+                <p>{this.state.data}</p>
             </div>
         );
     }
