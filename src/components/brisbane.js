f4ec993c8c0404a4c7070155bdbe2830c6e51b4c
ichpuchtli <sam.mack91@gmail.com>
2018-07-20 20:55:05 +1000

    make api request and render it and add type definition for api

--- src/components/brisbane.js
+++ src/components/brisbane.js
@@ -2,36 +2,65 @@ import React, { Component } from 'react';
 
 
 class Brisbane extends Component {
+
     constructor(props) {
         super(props);
+
         this.state = {
             title: 'Brisbane Test',
-            data: []
+            data: null
         };
     }
 
-    componentDidMount() {
-        /* fetch('https://api.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9',{
-        //fetch('https://samples.openweathermap.org/data/2.5/forecast?lat=24&lon=153&appid=dd0c79dc2c56fc2c2dbfe76ed03897d9', {
-            crossDomain: true,
-            method: 'POST',
-            mode: "no-cors",
-            headers: { 'Content-Type': 'application/json' }
-        }) */
-        fetch('http://api.apixu.com/v1/forecast.json?key=69e03ca47c2d4a998f593024181807&q=Brisbane&days=1')
+    getMeData(){
+        return fetch('http://api.apixu.com/v1/forecast.json?key=69e03ca47c2d4a998f593024181807&q=Brisbane&days=7')
             .then(res => res.json())
-            .then(data => this.setState( [data] , () => console.log('data: ', data)))
+    }
 
-            .catch(function (error) {
-                console.log('Looks like there was a problem: \n', error);
-            });
+    /**
+     * 
+     * 
+     * @param {Forecast.RootObject} data 
+     */
+    handleData(data)
+    {
+        this.setState({title: this.title, data});
+        console.log(data);
+    }
+
+
+    componentDidMount() {
+
+        this.getMeData()
+        .then(data => this.handleData(data))
+        .catch(e => this.handleError(e));
 
     }
+
+    handleError(error) {
+        console.log('Looks like there was a problem: \n', error);
+    }
+
     render() {
+
+        /** @type {Forecast.RootObject|null} */
+        var payload = this.state.data;
+
+        if(payload == null){
+            return (<p>Loading...</p>);
+        }
+
         return (
             <div>
                 <h1>{this.state.title}</h1>
-                <p>{this.state.data}</p>
+                <p>Feels like {payload.current.feelslike_c}</p>
+
+                <h1>Forecasts</h1>
+                <p>
+                    <ul>
+                    {payload.forecast.forecastday.map(fd => (<li>{fd.day.avgtemp_c}</li>))}
+                    </ul>
+                </p>
             </div>
         );
     }
